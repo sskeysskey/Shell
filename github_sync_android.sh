8eedfbcd5163da9d85ecad0729950e0b6100255a
sskeysskey <728308386@qq.com>
2025-07-31 22:28:31 +0800

    Auto commit at 2025-07-31 22:28:31

--- github_sync_android.sh
+++ github_sync_android.sh
@@ -9,3 +9,10 @@ current_time=$(date "+%Y-%m-%d %H:%M:%S")
 git add .
 git commit -m "Auto commit at $current_time"
 git push origin main
+
+# 输出结果
+if [ $? -eq 0 ]; then
+    echo "Successfully synced to GitHub at $current_time"
+else
+    echo "Error occurred while syncing to GitHub"
+fi
