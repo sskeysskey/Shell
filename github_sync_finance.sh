4499448bc52febc5f7ad9b4ef23d9aecbc6ae351
sskeysskey <728308386@qq.com>
2025-07-31 19:02:17 +0800

    Initial commit

---
+++ github_sync_finance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 设置工作目录
+cd /Users/yanzhang/Documents/Financial_System
+
+# 如果有 rebase 在进行，就 abort 掉
+if [ -d .git/rebase-apply ] || [ -d .git/rebase-merge ]; then
+  git rebase --abort
+fi
+
+# 获取当前时间作为commit信息
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+# 执行git操作
+git add .
+git commit -m "Auto commit at $current_time"
+git push origin main
+
+# 输出结果
+if [ $? -eq 0 ]; then
+    echo "Successfully synced to GitHub at $current_time"
+else
+    echo "Error occurred while syncing to GitHub"
+fi
---
+++ github_sync_sskeysskey.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 设置工作目录
+cd /Users/yanzhang/Documents/sskeysskey.github.io
+
+# 获取当前时间作为commit信息
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+# 执行git操作
+git add .
+git commit -m "Auto commit at $current_time"
+git push origin main
+
+# 输出结果
+if [ $? -eq 0 ]; then
+    echo "Successfully synced to GitHub at $current_time"
+else
+    echo "Error occurred while syncing to GitHub"
+fi
